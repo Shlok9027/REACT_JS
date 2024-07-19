478f87abdcb66ca03596151800f28cd789ca4c69
SHLOK KUMAR <shlok9027@gmail.com>
2024-07-19 20:23:39 +0530

    done with context and local storage

--- 08themeswitcher/src/components/Card.jsx
+++ 08themeswitcher/src/components/Card.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Card() {
   return (
-    <div className="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div className="w-full bg-slate-400 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
       <a href="/">
         <img
           className="p-8 rounded-t-lg"
@@ -11,7 +11,7 @@ export default function Card() {
       </a>
       <div className="px-5 pb-5">
         <a href="/">
-          <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+          <h5 className="text-lg font-bold tracking-tight text-gray-900 dark:text-white">
           Apple Watch Ultra 2 [GPS + Cellular 49mm]          </h5>
         </a>
         <div className="flex items-center mt-2.5 mb-5">
@@ -76,13 +76,13 @@ export default function Card() {
           </span>
         </div>
         <div className="flex items-center justify-between">
-          <span className="text-3xl font-bold text-gray-900 dark:text-white">
+          <span className="text-3xl font-bold text-stone-900 dark:text-white">
             ₹89900.00
           </span>
           <a
             href="/"
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 
-            font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700
+            className="text-white bg-stone-500 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 
+            font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-stone-
              dark:focus:ring-blue-800"
           >
             Add to cart
